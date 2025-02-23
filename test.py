d7dda6acac31c6ac7f3c02e162a2547cc7f64c14
Pollapaat  Suttimala <mudnott@gmail.com>
2025-02-23 21:17:02 +0700

    piper

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-import torch
-print("PyTorch Version:", torch.__version__)
-print("CUDA Available:", torch.cuda.is_available())
-print("cuDNN Version:", torch.backends.cudnn.version())
+if __name__ == "__main__":
+    from RealtimeTTS import TextToAudioStream, PiperEngine, PiperVoice
+
+    def dummy_generator():
+        yield "This is piper tts speaking."
+
+    voice = PiperVoice(
+        model_file="D:\WORK\Compettition\GRANITE HACKATHON\granite\piper\en_GB-southern_english_female-low.onnx",
+        config_file="D:\WORK\Compettition\GRANITE HACKATHON\granite\piper\en_GB-southern_english_female-low.onnx.json",
+    )
+
+    engine = PiperEngine(
+        piper_path="D:\WORK\Compettition\GRANITE HACKATHON\granite\piper",
+        voice=voice,
+    )
+
+    stream = TextToAudioStream(engine)
+    stream.feed(dummy_generator())
+    stream.play()
